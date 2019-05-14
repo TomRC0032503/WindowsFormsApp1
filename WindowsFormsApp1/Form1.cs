4810c77a4a0fa482fdcd708577981107b75519bc
RC0032503 <RC0032503@CRO-24-07>
2019-05-14 10:47:51 +0100

    display numbers

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -15,6 +15,26 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+            int i = 3;
+            richTextBox1.AppendText ("" + i);
+            double pi = 3.14159265;
+            double d1 = 5e6;
+            richTextBox1.AppendText(d1.ToString() + "\n");
+            richTextBox1.AppendText(d1.ToString("N") + "\n") ;
+            richTextBox1.AppendText(d1.ToString("N3") + "\n");
+            richTextBox1.AppendText(d1.ToString("N3") + "\n");
+
+            pi = 3.1411592653589793;
+            double c = 299792458;
+
+            richTextBox1.AppendText(pi.ToString("N3") + "\n");
+            richTextBox1.AppendText(pi.ToString("N5") + "\n");
+            richTextBox1.AppendText(pi.ToString("N15") + "\n");
+
+            richTextBox1.AppendText(c.ToString("N0") + "\n");
+
+
+
         }
     }
 }
